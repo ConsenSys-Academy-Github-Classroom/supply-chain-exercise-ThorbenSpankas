c6e9157d3a26198fc4556048339ed68e21f09472
ThorbenSpankas <thorben.spanka@estudiante.uam.es>
2021-10-19 12:28:17 +0200

    half done

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,27 +4,50 @@ pragma solidity >=0.5.16 <0.9.0;
 contract SupplyChain {
 
   // <owner>
+  address public owner;
 
   // <skuCount>
+  uint public skuCount;
 
   // <items mapping>
+  mapping (uint => Item) public items;
 
   // <enum State: ForSale, Sold, Shipped, Received>
+  enum State {
+    ForSale,
+    Sold,
+    Shipped,
+    Received
+  }
 
   // <struct Item: name, sku, price, state, seller, and buyer>
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    // is this referring to enum State? Otherwise there is no state datatype
+    // the reason is that in the test js they ask for
+    // isType(subjectStruct)("state")("State"), 
+    //      "`state` should be of type `State`"
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
   
   /* 
    * Events
    */
 
   // <LogForSale event: sku arg>
+  event LogForSale(uint sku);
 
   // <LogSold event: sku arg>
-
+  event LogSold(uint sku);
   // <LogShipped event: sku arg>
+  event LogShipped(uint sku);
 
   // <LogReceived event: sku arg>
-
+  event LogReceived(uint sku);
 
   /* 
    * Modifiers
@@ -33,14 +56,19 @@ contract SupplyChain {
   // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
 
   // <modifier: isOwner
+  modifier isOwner {
+    require(owner == msg.sender);
+    _;
+  }
 
   modifier verifyCaller (address _address) { 
-    // require (msg.sender == _address); 
+    require (msg.sender == _address); 
     _;
   }
 
   modifier paidEnough(uint _price) { 
     // require(msg.value >= _price); 
+    require(msg.value >= _price); 
     _;
   }
 
@@ -48,8 +76,11 @@ contract SupplyChain {
     //refund them after pay for item (why it is before, _ checks for logic before func)
     _;
     // uint _price = items[_sku].price;
+    uint _price = items[_sku].price;
     // uint amountToRefund = msg.value - _price;
+    uint amountToRefund = msg.value - _price;
     // items[_sku].buyer.transfer(amountToRefund);
+    items[_sku].buyer.transfer(amountToRefund);
   }
 
   // For each of the following modifiers, use what you learned about modifiers
@@ -61,13 +92,35 @@ contract SupplyChain {
   // an Item has been added?
 
   // modifier forSale
-  // modifier sold(uint _sku) 
+  modifier forSale(uint _sku) {
+    require(items[_sku].state == State.ForSale, "Item not for sale yet");
+    // the _; causes the code to first check the condition
+    // and afterwards execute the functions that is represented by _;
+    // in this case any function that has the condition forSale 
+    _;
+  }
+  // modifier sold(uint _sku)
+  // State or state?
+  modifier sold(uint _sku) {
+    require(items[_sku].state == State.Sold, "Item not sold yet");
+    _;
+  } 
   // modifier shipped(uint _sku) 
-  // modifier received(uint _sku) 
+  modifier shipped(uint _sku) {
+    require(items[_sku].state == State.Shipped, "Item not shipped yet");
+    _;
+  }
+  // modifier received(uint _sku)
+  modifier received(uint _sku) {
+    require(items[_sku].state == State.Received, "Item not received yet");
+    _;
+  } 
 
   constructor() public {
     // 1. Set the owner to the transaction sender
+    owner = msg.sender;
     // 2. Initialize the sku count to 0. Question, is this necessary?
+    skuCount = 0;
   }
 
   function addItem(string memory _name, uint _price) public returns (bool) {
@@ -119,15 +172,16 @@ contract SupplyChain {
   function receiveItem(uint sku) public {}
 
   // Uncomment the following code block. it is needed to run tests
-  /* function fetchItem(uint _sku) public view */ 
-  /*   returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) */ 
-  /* { */
-  /*   name = items[_sku].name; */
-  /*   sku = items[_sku].sku; */
-  /*   price = items[_sku].price; */
-  /*   state = uint(items[_sku].state); */
-  /*   seller = items[_sku].seller; */
-  /*   buyer = items[_sku].buyer; */
-  /*   return (name, sku, price, state, seller, buyer); */
-  /* } */
+  //
+  //function fetchItem(uint _sku) public view  
+  ///*   returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) 
+  //{ 
+  //  name = items[_sku].name;
+  //  sku = items[_sku].sku; 
+  //  price = items[_sku].price; 
+  //  state = uint(items[_sku].state); 
+  //  seller = items[_sku].seller;
+  //  buyer = items[_sku].buyer; 
+  //  return (name, sku, price, state, seller, buyer); 
+  // }
 }
